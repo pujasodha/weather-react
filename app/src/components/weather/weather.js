2160de800b21ecec46cad1c1b3dc401a09b5c11f
pujasodha <pujasodha@gmail.com>
2018-12-12 16:28:43 -0800

    fixed small styling issue

--- app/src/components/weather/weather.js
+++ app/src/components/weather/weather.js
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import './weather.css'
 class Weather extends React.Component {
 
     render  () {
         return (
-            <div>
-                {this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
-                {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
-                {this.props.description && <p>Conditions: {this.props.description}</p>}
+            <div className = 'weather-display'>
+                {this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>} {"\n"}
+                {this.props.temperature && <p>Temperature: {this.props.temperature}</p>} {"\n"}
+                {this.props.description && <p>Conditions: {this.props.description}</p>} {"\n"}
                 {this.props.error && <p>{this.props.error}</p>}
             </div>
         )
