8dbc7b5f2336b150c19116533149b2901c68580e
pujasodha <pujasodha@gmail.com>
2018-12-18 08:42:31 -0800

    build

---
+++ build/precache-manifest.fae6ffa62361b8dfac4f67d0b5e4bed3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "34a9f3f9db67e79bd90d",
+    "url": "/static/js/main.34a9f3f9.chunk.js"
+  },
+  {
+    "revision": "dedf7abc42a50e8e118e",
+    "url": "/static/js/1.dedf7abc.chunk.js"
+  },
+  {
+    "revision": "34a9f3f9db67e79bd90d",
+    "url": "/static/css/main.8465ae33.chunk.css"
+  },
+  {
+    "revision": "df9a6245ee320d0612b7f9d8d3e70d83",
+    "url": "/index.html"
+  }
+];
